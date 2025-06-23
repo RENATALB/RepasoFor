ad9cec17ba3b7481c7e71b3728d4806e519647aa
rena <166546709+RENATALB@users.noreply.github.com>
2025-06-23 11:31:07 -0300

    random color básico hecho

---
+++ Assets/ColorManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour
+{
+    public Color[] colors;
+    public Renderer cubeRenderer;
+    public int randomColorIndex;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomColorIndex = Random.Range(0, colors.Length);
+        cubeRenderer.material.color = colors[randomColorIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
